e3d1fbdb59b24b7311ceb1be39a832263255c788
tornadochicken <tornadochicken@live.com>
2013-10-30 15:06:04 +0100

    Create spill.php

---
+++ struktur/spill.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <article>
+            <h1>Dealer:</h1>
+            <div id="dealer-space"></div>
+            <div id="sum-dealer"></div>
+            <div id="stake-dealer"></div>
+            
+            <h1>You:</h1>
+            <div id="player-space"></div>
+            <div id="sum-player"></div>
+            <div id="staker-player"></div>
+            <button id="Hit">Hit</button>
+            <button id="Hold">Hold</button>
+            <button id="Double-Down">Double Down</button>
+        </article>
+    </body>
+</html>
