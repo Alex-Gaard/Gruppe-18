6d2707b2ed7ad8f6dd30721029223c169e457489
Olav Andreas Måren <s198548@stud.hioa.no>
2013-10-31 15:34:45 +0100

    Lenker opp ny .js fil
    Lenker til javascript som skal manipulere kortene.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <title>lectureGaming</title>
         <meta charset=UTF-8/>
         <link rel="stylesheet" type="text/css" href="./struktur/casino.css" title="Casino" />
+        <script src="./kode/manipulere_spillerområde.js"></script>
     </head>
     <body>
 <?php
@@ -11,7 +12,7 @@ session_start();
 
 //hver del av siden ligger i egen fil slik at flere kan redigere simultant
 include './struktur/header.html';
-include './innhold/spill.php';
+include './struktur/spill.php';
 include './struktur/footer.html';
 
 ?>
