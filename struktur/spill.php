01e22c9cc1ccc2db14dde2f60d3c5456f9806927
Olav Andreas Måren <s198548@stud.hioa.no>
2013-10-31 14:55:08 +0100

    Feil i img-element
    Hadde skrevet href og ikke src inne i img-element... =P
    I denne versjonen er det lagt til 5 kort til både dealer og spiller for å teste css og layout.

--- struktur/spill.php
+++ struktur/spill.php
@@ -8,22 +8,22 @@
         <article>
             <h1>Dealer:</h1>
             <div id="dealer-space">
-            <img href="./cards/c1.png" alt="Ace of Clubs" id="dealer_card_1" class="card_right card">
-            <img href="./cards/h1.png" alt="Ace of Hearts" id="dealer_card_2" class="card_right card">
-            <img href="./cards/d1.png" alt="Ace of Diamonds" id="dealer_card_3" class="card_under_right card">
-            <img href="./cards/s1.png" alt="Ace of Spades" id="dealer_card_4" class="card_under_right card">
-            <img href="./cards/s4.png" alt="Four of Spades" id="dealer_card_5" class="card_under_right card">
+            <img src="./cards/s4_under.png" alt="Four of Spades" id="dealer_card_5" class="card_under_right card">
+            <img src="./cards/s1_under.png" alt="Ace of Spades" id="dealer_card_4" class="card_under_right card">
+            <img src="./cards/d1_under.png" alt="Ace of Diamonds" id="dealer_card_3" class="card_under_right card">
+            <img src="./cards/h1.png" alt="Ace of Hearts" id="dealer_card_2" class="card_right card">
+            <img src="./cards/c1.png" alt="Ace of Clubs" id="dealer_card_1" class="card_right card">
             </div>
             <div id="cards_sum_dealer"></div>
             <div id="stake_player1" class="cash"></div>
             
             <h1>You:</h1>
             <div id="player-space">
-            <img href="./cards/c1.png" alt="Ace of Clubs" id="player_card_1" class="card_left card">
-            <img href="./cards/h1.png" alt="Ace of Hearts" id="player_card_2" class="card_left card">
-            <img href="./cards/d1.png" alt="Ace of Diamonds" id="player_card_3" class="card_under_left card">
-            <img href="./cards/s1.png" alt="Ace of Spades" id="player_card_4" class="card_under_left card">
-            <img href="./cards/s4.png" alt="Four of Spades" id="player_card_5" class="card_under_left card">
+            <img src="./cards/s4_under.png" alt="Four of Spades" id="player_card_5" class="card_under_left card">
+            <img src="./cards/s1_under.png" alt="Ace of Spades" id="player_card_4" class="card_under_left card">
+            <img src="./cards/d1_under.png" alt="Ace of Diamonds" id="player_card_3" class="card_under_left card">
+            <img src="./cards/h1.png" alt="Ace of Hearts" id="player_card_2" class="card_left card">
+            <img src="./cards/c1.png" alt="Ace of Clubs" id="player_card_1" class="card_left card">
             </div>
             <div id="cards_sum_player"></div>
             <div id="bank_player1" class="cash"></div>
