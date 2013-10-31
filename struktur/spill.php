e4c7efa0d8aadacce9dc497df1522f986f2c5d97
Olav Andreas Måren <s198548@stud.hioa.no>
2013-10-31 11:41:48 +0100

    Nye klasser
    Legger til klasser som skal manipuleres i CSS.

--- struktur/spill.php
+++ struktur/spill.php
@@ -8,26 +8,25 @@
         <article>
             <h1>Dealer:</h1>
             <div id="dealer-space">
-            <img id="dealer-1">
-            <img id="dealer-2">
-            <img id="dealer-3">
-            <img id="dealer-4">
-            <img id="dealer-5">
+            <img href="./cards/c1.png" alt="Ace of Clubs" id="dealer_card_1" class="card_right card">
+            <img href="./cards/h1.png" alt="Ace of Hearts" id="dealer_card_2" class="card_right card">
+            <img href="./cards/d1.png" alt="Ace of Diamonds" id="dealer_card_3" class="card_under_right card">
+            <img href="./cards/s1.png" alt="Ace of Spades" id="dealer_card_4" class="card_under_right card">
+            <img href="./cards/s4.png" alt="Four of Spades" id="dealer_card_5" class="card_under_right card">
             </div>
-            <div id="sum-dealer"></div>
-            <div id="stake-dealer"></div>
+            <div id="cards_sum_dealer"></div>
+            <div id="stake_player1" class="cash"></div>
             
             <h1>You:</h1>
             <div id="player-space">
-            <img id="player-1">
-            <img id="player-2">
-            <img id="player-3">
-            <img id="player-4">
-            <img id="player-5">
+            <img href="./cards/c1.png" alt="Ace of Clubs" id="player_card_1" class="card_left card">
+            <img href="./cards/h1.png" alt="Ace of Hearts" id="player_card_2" class="card_left card">
+            <img href="./cards/d1.png" alt="Ace of Diamonds" id="player_card_3" class="card_under_left card">
+            <img href="./cards/s1.png" alt="Ace of Spades" id="player_card_4" class="card_under_left card">
+            <img href="./cards/s4.png" alt="Four of Spades" id="player_card_5" class="card_under_left card">
             </div>
-            <div id="sum-player"></div>
-            <div id="stake-player"></div>
-            <div id="bank"></div>
+            <div id="cards_sum_player"></div>
+            <div id="bank_player1" class="cash"></div>
             <button id="Hit">Hit</button>
             <button id="Hold">Hold</button>
             <button id="Double-Down">Double Down</button>
