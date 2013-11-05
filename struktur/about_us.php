b3f9e993e11831d846f87fb6710e179776c1bd59
tornadochicken <tornadochicken@live.com>
2013-11-05 21:28:00 +0100

    Create about_us.php
    Lagde siden <about us> og skrev in et par ord om oss

---
+++ struktur/about_us.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About us</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="./casino.css" title="Casino">
+    </head>
+    <body>
+        <?php
+session_start();
+include './struktur/header.html';
+include './struktur/footer.html';
+?>
+<p>We are four students from Oslo and Akershus University College of Applied Sciences, we we're grouped together by interests in this task and set to make something with what we have learned from this class.
+We decided to at least make one functional card game which will include a high-score board for all the users so they can compete over the top places.<br>
+We might make a second type of card game since we don't know how complex and time consuming the blackjack game will be. 
+But our main goal is to finish and make the first game as good as possible before we start on something else.
+
+Our homepages:
+<a href="http://student.cs.hioa.no/~s198526"> Tobias W. Viulsrød</a>
+<a href="http://student.cs.hioa.no/~s198548"> Olav A. Måren </a>
+<a href="http://student.cs.hioa.no/~s198585/webprosjekt2"> Alexander O. Gård </a> 
+<a href="http://student.cs.hioa.no/~s198736"> Sindre Tvinnereim </a>
+</p>
+
+    </body>
+</html>
