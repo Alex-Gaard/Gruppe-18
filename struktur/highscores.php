ffb51f479c2b03c510ae023ab047842f703970c5
tornadochicken <tornadochicken@live.com>
2013-11-05 22:13:16 +0100

    Update highscores.php
    Re-arranged

--- struktur/highscores.php
+++ struktur/highscores.php
@@ -6,11 +6,13 @@
         <link rel="stylesheet" type="text/css" href="./casino.css" title="Casino">
     </head>
     <body>
-            <h2>High scores<h2>
         <?php
 session_start();
-include './struktur/header.html';
-include './struktur/footer.html';
+include 'header.html';
+?>
+            <h2>High scores</h2>
+        <?php
+include 'footer.html';
 ?>
     </body>
 </html>
