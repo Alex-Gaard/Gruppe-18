9cdcded69bf36e696545bc256c2451aaa8b993fc
tornadochicken <tornadochicken@live.com>
2013-11-05 21:43:12 +0100

    Update about_us.php

--- struktur/about_us.php
+++ struktur/about_us.php
@@ -11,7 +11,7 @@ session_start();
 include './struktur/header.html';
 include './struktur/footer.html';
 ?>
-<p>We are four students from Oslo and Akershus University College of Applied Sciences, we we're grouped together by interests in this task and set to make something with what we have learned from this class.
+<p id="about_us">We are four students from Oslo and Akershus University College of Applied Sciences, we we're grouped together by interests in this task and set to make something with what we have learned from this class.
 We decided to at least make one functional card game which will include a high-score board for all the users so they can compete over the top places.<br>
 We might make a second type of card game since we don't know how complex and time consuming the blackjack game will be. 
 But our main goal is to finish and make the first game as good as possible before we start on something else.
