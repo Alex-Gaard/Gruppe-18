0fd7a7396250feaf163d37e2fabd6e4d7fffed11
tornadochicken <tornadochicken@live.com>
2013-11-05 22:14:12 +0100

    Update about_us.php
    Re-arranged

--- struktur/about_us.php
+++ struktur/about_us.php
@@ -8,20 +8,21 @@
     <body>
         <?php
 session_start();
-include './struktur/header.html';
-include './struktur/footer.html';
+include 'header.html';
 ?>
-<p id="about_us">We are four students from Oslo and Akershus University College of Applied Sciences, we we're grouped together by interests in this task and set to make something with what we have learned from this class.
-We decided to at least make one functional card game which will include a high-score board for all the users so they can compete over the top places.<br>
-We might make a second type of card game since we don't know how complex and time consuming the blackjack game will be. 
-But our main goal is to finish and make the first game as good as possible before we start on something else.
-
+<p id="about_us">We are four students from Oslo and Akershus University College of Applied Sciences, we we're grouped together by interests in this task and set to make something with what we have learned from this class.<br>
+For this task we decided to at least make one functional card game which will include a high-score board for all the users so they can compete over the top places.<br>
+Later we might make a second type of card game since we don't know how complex and time consuming the blackjack game will be. 
+But our main goal is to finish and make the first game as good as possible before we start on something else.<br>
+<br>
 Our homepages:
-<a href="http://student.cs.hioa.no/~s198526"> Tobias W. Viulsrød</a>
-<a href="http://student.cs.hioa.no/~s198548"> Olav A. Måren </a>
-<a href="http://student.cs.hioa.no/~s198585/webprosjekt2"> Alexander O. Gård </a> 
-<a href="http://student.cs.hioa.no/~s198736"> Sindre Tvinnereim </a>
+<br><a href="http://student.cs.hioa.no/~s198526"> Tobias W. Viulsrød</a><br>
+<a href="http://student.cs.hioa.no/~s198548"> Olav A. Måren </a><br>
+<a href="http://student.cs.hioa.no/~s198585/webprosjekt2"> Alexander O. Gård </a><br>
+<a href="http://student.cs.hioa.no/~s198736"> Sindre Tvinnereim </a><br>
 </p>
-
+       <?php
+include 'footer.html';
+?>
     </body>
 </html>
