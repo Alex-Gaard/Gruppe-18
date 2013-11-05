c081fe01e03a06487404bffa06e99c2df7439504
tornadochicken <tornadochicken@live.com>
2013-11-05 21:27:03 +0100

    Create highscores.php
    Lagde filen for High scores siden...

---
+++ struktur/highscores.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Highscores</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="./casino.css" title="Casino">
+    </head>
+    <body>
+            <h1>High scores<h1>
+        <?php
+session_start();
+include './struktur/header.html';
+include './struktur/footer.html';
+?>
+    </body>
+</html>
