3c912e0984363538afceafdf528e5ae23e867bc2
sindret <sindretvinner@hotmail.com>
2013-11-06 12:03:02 +0100

    Alpha versjon av script til bruk ved login.
    Lagt til endel testfunksjoner for å se om scriptet fungerer som ment.
    Er knyttet til filene login.html og cookie.js.

---
+++ login.js
@@ -0,0 +1,118 @@
+
+function log( name ){
+	
+	document.getElementById("log").innerHTML="Welcome back ";
+
+}
+
+//Validates that all fields in the form are filled out.
+function validateForm( name, password ){
+	
+	var x = name; //Sets x as the value of name field.
+	var y = password; //Sets y as the value of password field.
+	
+	//Checks if any of the fields are empty and gives a message to the user.
+	if( x == null || x == "" || y == null || y == ""){
+	
+		alert("Please fill out all fields of the form!");
+		return;
+	
+	}
+	
+	if( getCookieName( x ) == -1 ){
+		
+		var answer = prompt( "Cannot find user: " + x + "\nDo you want to create a new user with name " + x + " and password " + y + "\n y/n ?" );
+		
+		//Creates a new user if the answer is yes.
+		if( answer == "y" ){
+		
+			createCookie( x, y );
+			return;
+			
+		}
+		
+		//Exits function if answer is something other than yes.
+		if( answer != "y" ){
+		
+			return;
+		
+		}
+		
+	
+	}
+	
+	//Checks if a cookie exists.
+	if( getCookieName( x ) != -1 ){
+		
+		//Checks if the name that the user logged in with is the same as username in cookie.
+		if( getCookieName( x ) == x ){
+			
+			//Checks if the password that the user logged in with is the same as password stored in cookie.
+			if( getCookiePassword( x ) == y ){
+				
+				log( x );
+				alert( "Welcome back " +  x );
+				
+				return;
+				
+			}
+			
+			if( getCookiePassword( x ) != y ){
+			
+				alert( "Wrong password! for user " + x );
+				return;
+				
+			}
+		
+		}
+		
+    }
+	
+	//If a cookie doesnt already exists, creates a new cookie with chosen username and password.
+	if( getCookieName( x ) == -1 ){
+		
+		createCookie( x, y );
+		
+		alert( "A new user was created.\n" + "Username: " + x + "\nPassword: " + y );
+	
+	}
+	
+
+}//End function validateForm().
+
+//Use to check what information is stored in the cookie.
+function getInfo(){
+
+	alert( document.cookie.toString() );
+
+}
+
+function getName(){
+	
+	var name = "Sindres";
+	
+	alert( getCookieName( name ) );
+}
+
+function getPassword(){
+
+	var x = "Sindres";
+	
+	alert( getCookiePassword( x ) );
+	
+}
+
+function getScore(){
+
+	alert( getCookieScore("Sindres") );
+
+}
+
+function setScore(){
+	
+	var name = "Sindre";
+	var score = 200;
+	
+	setCookieScore( name, score );
+
+}
