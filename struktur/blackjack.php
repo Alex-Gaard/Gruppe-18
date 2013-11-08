c93ad5767c4c5f3c2d8bdabead26ea9ab520a360
Olav Andreas Måren <s198548@stud.hioa.no>
2013-11-08 11:52:05 +0100

    Gjør klar "bordet" for skript
    Legger til riktig kort og knapper som er forenelige med skriptets behov. Navngir pengesummene med egne span elementer for at de skal kunne bli manipulert på en grei måte.

--- struktur/blackjack.php
+++ struktur/blackjack.php
@@ -13,34 +13,23 @@ include 'header.html';
         <article>
             <h1>Dealer:</h1>
             <div id="dealer-space">
-            <img src="./cards/s4_under.png" alt="Four of Spades" id="dealer_card_5" class="card_under_right card">
-            <img src="./cards/s1_under.png" alt="Ace of Spades" id="dealer_card_4" class="card_under_right card">
-            <img src="./cards/d1_under.png" alt="Ace of Diamonds" id="dealer_card_3" class="card_under_right card">
-            <img src="./cards/h1.png" alt="Ace of Hearts" id="dealer_card_2" class="card_right card">
-            <img src="./cards/c1.png" alt="Ace of Clubs" id="dealer_card_1" class="card_right card">
+            <img src="./cards/git.png" alt="Deck with picture og Git Squid" id="backside" class="card_right card">
             </div>
             <div id="cards_sum_dealer"></div>
-            <fieldset id="stake_player1" class="cash">
+            <fieldset id="stake_boks" class="cash">
                 <legend>Stake</legend>
-                $35
+                $<span id="stake_player_1"></stake>
             </fieldset>
             
             <h1>You:</h1>
             <div id="player-space">
-            <img src="./cards/s4_under.png" alt="Four of Spades" id="player_card_5" class="card_under_left card">
-            <img src="./cards/s1_under.png" alt="Ace of Spades" id="player_card_4" class="card_under_left card">
-            <img src="./cards/d1_under.png" alt="Ace of Diamonds" id="player_card_3" class="card_under_left card">
-            <img src="./cards/h1.png" alt="Ace of Hearts" id="player_card_2" class="card_left card">
-            <img src="./cards/c1.png" alt="Ace of Clubs" id="player_card_1" class="card_left card">
             </div>
             <div id="cards_sum_player"></div>
-            <fieldset id="bank_player1" class="cash">
+            <fieldset id="bank_boks" class="cash">
                 <legend>Bank</legend>
-                $200
+                $<span id="bank_player_1">200</span>
             </fieldset>
-            <button id="Hit">Hit</button>
-            <button id="Hold">Hold</button>
-            <button id="Double-Down">Double Down</button>
+            <button id="Deal" onclick="deal()">Deal</button>
         </article>
         <?php
 include 'footer.html';
