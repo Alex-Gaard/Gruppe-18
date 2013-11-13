98b303ead8788dab28d91f78a108f22b8b9a06a7
tornadochicken <tornadochicken@live.com>
2013-11-13 17:23:30 +0100

    Create rules.php
    Spellchecked

---
+++ struktur/rules.php
@@ -0,0 +1,32 @@
+<article>
+    <h1>BlackJack Rules</h1>
+    <p>Blackjack, also known as twenty-one, is the most widely played casino 
+    banking game in the world.[1] Blackjack is a comparing card game between a 
+    player and dealer, meaning that players compete against the dealer but not 
+    against any other players. It is played with one or more decks of 52 cards. 
+    The object of the game is to "beat the dealer", which can be done in a 
+    number of ways:</p>
+    <ul>
+        <li>Get 21 points on your first two cards (called a blackjack), without a dealer blackjack;</li>
+        <li>Reach a final score higher than the dealer without exceeding 21; or</li>
+        <li>Let the dealer draw additional cards until his hand exceeds 21.</li>
+    </ul>
+    <p>The player or players are dealt an initial two-card hand and add together 
+    the value of their cards. Face cards (kings, queens, and jacks) are counted 
+    as ten points. A player and the dealer can count his or her own ace as 1 
+    point or 11 points. All other cards are counted as the numeric value shown 
+    on the card. After receiving their initial two cards, players have the 
+    option of getting a "hit", or taking an additional card. In a given round, 
+    the player or the dealer wins by having a score of 21 or by having the 
+    highest score that is less than 21. Scoring higher than 21 (called "busting" 
+    or "going bust") results in a loss. A player may win by having any final 
+    score equal to or less than 21 if the dealer busts</p>
+    <p>If a player holds an ace valued as 11, the hand is called "soft", meaning 
+    that the player cannot go bust by taking an additional card; otherwise, the 
+    hand is "hard". The dealer has to take hits until his or her cards total 17 
+    or more points. (In some casinos, the dealer also hits on a "soft" 17—e.g., 
+    an initial ace and six.)  If the player and dealer have the same point total, 
+    this is called a "push", and the player typically does not win or lose money 
+    on that hand.</p>
+    <p><strong>Source: Wikipedia</strong><a href="http://en.wikipedia.org/wiki/Blackjack" alt="Wikipedias page on BlackJack">/wiki/Blackjack</a>.</p>
+</article>
