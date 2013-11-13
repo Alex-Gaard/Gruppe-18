706e573c82c20ed8b648794687f5c1e1b5e82655
tornadochicken <tornadochicken@live.com>
2013-11-13 17:23:13 +0100

    Create tutorial.php
    Spellchecked

---
+++ struktur/tutorial.php
@@ -0,0 +1,70 @@
+<article>
+    <h1>Tutorial lectureBlackJack</h1>
+    <p>
+        In order to play, you have to log in. By logging in you agree to our 
+        <a href="?page=terms">terms of service.</a> They simply state that we
+        set a cookie to track your high score, so that it is displayed next time
+        you log in.
+    </p>
+    <p>
+        The game follows regular BlackJack rules as stated in Wikipedia. You 
+        click the Deal button to get cards, the game then deals two cards for 
+        both you and the dealer. If you get BlackJack, that is a sum of 21, you
+        instantly win!
+    </p>
+    <p>
+        One of the dealers first cards is hidden from your view. You must decide 
+        if you want to stand with the cards you allready got or draw more cards.
+        Getting more cards is called 'hit', and you do this by clicking the Hit
+        button. Otherwise you click the Stand button, to hold.
+    </p>
+    <p>
+        There is a third option as well. It is called 'double down'. Clicking on
+        the Double Down button, you increase your stakes by 100% and get one -1-
+        extra card. You then automatically stand and the dealer draws.
+    </p>
+    <h2>Winning and loosing your bet</h2>
+    <p>
+        If your cards sum up to 21, you got BlackJack! This means you immediately
+        win 3:2 of your bettings. Then you can play again, by clicking the Play
+        Again button.
+    </p>
+    <p>
+        If you get more than 21, this is a bust. You loose your bettings and can
+        play again. Clicking the Play Again button always allows you to get new
+        cards.
+    </p>
+    <p>
+        If you stand with a sum less than 21, the dealers draw cards. The dealer
+        draws until she wins, it's a tie or she busts. If the dealer busts, you 
+        win 2:1 of your bettings. If it's a tie, you both have the same sum, you
+        get your bettings back.
+    </p>
+    <h1>Changing settings and game rules</h1>
+    <p>
+        In the <a href="?page=settings">Settings page</a> you can change three 
+        game settings. This alters the rules of the game. The first setting is
+        the number of decks the dealer draws from. More decks means the same 
+        card can be drawn twice or more, and makes counting cards harder. This
+        setting is set to 1 deck by default.
+    </p>
+    <h2>Soft 17</h2>
+    <p>
+        By enabling 'Soft 17' the dealer automatically stands when she gets a 
+        sum of 17 or higher. In the normal game rules she always draws until 
+        she wins or busts, regardless of the sum she has. The soft part is that
+        if she gets 17 AND has drawn an ace, she continues to draw.
+    </p>
+    <p>
+        If the dealer stands at 18, and you got 19, you win. This increases your
+        chance of winning, albeit by a small margin. This rule is disabled by 
+        default.
+    </p>
+    <h2>5 card win</h2>
+    <p>
+        Due to the small probability to draw as many as 5 cards without busting,
+        some rules treat this as a win. By enabling this setting you win if you 
+        click the Hit button until you are given 5 cards and still don't bust. 
+        This setting is disabled by default.
+    </p>
+</article>
