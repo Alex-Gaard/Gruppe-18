e6f8e8484ca720743e55305dff8c2caee6b9bcf3
Olav Andreas Måren <s198548@stud.hioa.no>
2013-11-14 13:57:05 +0100

    Create settings.php

---
+++ struktur/settings.php
@@ -0,0 +1,21 @@
+<article>
+    <h1>Game settings</h1>
+    <p>
+        Number of decks: 
+    <select name='nrDecks'>
+        <option value='1'>1 deck</option>
+        <option value='2'>2 decks</option>
+        <option value='3'>3 decks</option>
+        <option value='4'>4 decks</option>
+        <option value='5'>5 decks</option>
+        <option value='6'>6 decks</option>
+        <option value='7'>7 decks</option>
+        <option value='8'>8 decks</option>
+    </select><br/>
+        <input name='softSeventeen' type='checkbox'> Soft 17 <br/>
+        <input name='fiveCardWin' type='checkbox'> 5 card win <br/>
+        <br/>
+        <button class='centerbutton' onclick='setSettings()'>Set settings</button>
+    </p>
+    <hr id='hr_bottom'/>
+</article>
