13e57b6468e4e347894fb21803f3f3733f817b15
tornadochicken <tornadochicken@live.com>
2013-11-19 19:23:55 +0100

    Update policy.php

--- struktur/policy.php
+++ struktur/policy.php
@@ -1,6 +1,7 @@
 <article>
 <h1>Our Policy</h1>
-<p>By playing our game you agree to us making a cookie for you, your user will then be connected to your score so you can appear on the high-scores.<br>
-   We have made a button which deletes this cookie for you, it can be found on the frontpage or you can manually delete it through your browser if you know how.<br>
+<p>By playing our game you agree with us making a cookie for you which will allow you to make full use of our game.<br>
+   Your user will then be connected to your score so you can appear on the high-scores.<br>
+   We have made a button which deletes this cookie for you, it can be found on the login page or you can manually delete it through your browser if you know how.<br>
 </p>
 </article>
