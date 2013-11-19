2c4fb6816abd3e491d47b5c2d5bea15a6d7a9482
tornadochicken <tornadochicken@live.com>
2013-11-19 19:34:54 +0100

    Create credits.php
    Skal vi ha noe mer utdyppende tekst? Slå sammen denne og about_us?

---
+++ struktur/credits.php
@@ -0,0 +1,12 @@
+<article>
+    <h1>Credits</h1>
+    <p>
+        The HTML structure of this page, cascading style sheets and code is all
+        written by the <a href="?page=about_us">authors</a>. Since they attend
+        different courses at the same University College, they have diffrent profiencies in each
+        programming language (Java, PHP etc.). The choice therefore was to allow everyone to use
+        their skills to the best of their abilities by giving tasks based out of this. Though
+        we cooperated like a team through the whole process helping each other out the best we could
+        when one of us was stuck.
+    </p>
+</article>
