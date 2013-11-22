081d117676c8125a13dd87065355ee8496864896
Olav Andreas Måren <s198548@stud.hioa.no>
2013-11-22 13:40:07 +0100

    Revidert meny
    Meny med alle nye undersider.

--- index.php
+++ index.php
@@ -4,31 +4,34 @@
         <title>lectureGaming</title>
         <meta charset="UTF-8" />
         <link rel="stylesheet" type="text/css" href="./struktur/casino.css" title="Casino" />
-        <script src="./kode/manipulere_spilleromraade.js"></script>
+        <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.min.js"></script>
+        <script charset="utf-8" type="text/javascript" src="./kode/spillmotor.js"></script>
+	<script charset="utf-8" type="text/javascript" src="./kode/login.js"></script>
+	<script charset="utf-8" type="text/javascript" src="./kode/cookie_single.js"></script>
+	<script charset="utf-8" type="text/javascript" src="./kode/loggedinas.js"></script>
     </head>
     <body>
-<header>
-    <a href="index.php"><h1>lectureGaming</h1></a>
-    <nav>
-        <h1>Main Menu</h1>
-        <ul>
-            <li><a href="struktur/blackjack.php">lectureBlackJack</a></li>
-            <li><a href="struktur/highscores.php">High Scores</a></li>
-            <li><a href="struktur/about_us.php">About Us</a></li>
-        </ul>
-        <hr/>
-    </nav>
-</header>
-        <h2>Welcome to lectureBlackjack</h2>
-        <h2>~ When the lecture calls for it ~</h2><br>
-        <img src="lectureGames_forside.gif" id="forsidebilde"><br><br>
-        <div id="centerbutton">
-            <button class="centerbutton">Logg inn</button>
-            <button class="centerbutton">Tutorial/Help</button><br><br>
-        </div> 
-   <?php
-session_start();
-include './struktur/footer.html';
+<?php
+include './struktur/header.html'; //Sidenavn og meny
+
+//Mater inn hovedinnhold:
+if (isset($_REQUEST['page'])) { //sjekk om ?page er angitt i URL
+    $page = $_REQUEST['page'];
+    if ($page == "0" || $page == "login") {include './struktur/login.php';}
+    elseif ($page == 1 || $page == "BlackJack") {include './struktur/blackjack.php';}
+    elseif ($page == 2 || $page == "tutorial") {include './struktur/tutorial.php';}
+    elseif ($page == 3 || $page == "rules") {include './struktur/rules.php';}
+    elseif ($page == 4 || $page == "settings") {include './struktur/settings.php';}
+    elseif ($page == 5 || $page == "highscores") {include './struktur/highscore.php';}
+    elseif ($page == 6 || $page == "about_us") {include './struktur/about_us.php';}
+    elseif ($page == 7 || $page == "terms") {include './struktur/policy.php';}
+    elseif ($page == 8 || $page == "credits") {include './struktur/credits.html';}
+    elseif ($page == 9 || $page == "login_new") {include './struktur/login.html';}
+    else {include './struktur/start.html';} //hvis feil page-verdi blir angitt
+}else {include './struktur/start.html';} //hvis ingen page-verdi er angitt
+//Slutt hovedinnhold
+
+include './struktur/footer.html'; //copyright notice
 ?>
     </body>
 </html>
