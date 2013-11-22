6cb1ac01564a5d46af763270a350be539f931a95
tornadochicken <tornadochicken@live.com>
2013-11-22 12:24:34 +0100

    Update login.php
    Fjernet unødvendige </input> tags

--- struktur/login.php
+++ struktur/login.php
@@ -2,8 +2,8 @@
 <h1>Form value:</h1>
 	<form name="login" onsubmit="validateForm( namefield.value, passwordfield.value )" method="POST" action="javascript:void(0)">
 	
-	  Name:     <input type="text" required name="namefield"></input><br>
-	  Password: <input type="password" required name="passwordfield"></input>
+	  Name:     <input type="text" required name="namefield"><br>
+	  Password: <input type="password" required name="passwordfield">
 	            <input type="submit" value="Log in"><br/>
                     <input type="checkbox" required name="terms_of_service">
                     Accept our <a href='?page=terms'>terms and conditions</a>.
